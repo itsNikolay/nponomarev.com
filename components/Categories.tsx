6feb8b77a475f41ab6fb0b9c3cc7f66c34dc8b37
Nikolay Ponomarev <itsnikolay@gmail.com>
2020-12-15 00:55:52 +0300

    styles for categories

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -3,14 +3,15 @@ import Link from 'next/link'
 
 const Categories = () => {
   return (
-    <div className="inline-flex space-x-4">
+    <div className="flex max-w-full pt-6 pb-6 text-lg border-b-2 space-x-4">
       { categoriesList.map(({ name, path }) =>
       <Link href={path} key={path}>
-        <a aria-label={name}>
+        <a aria-label={name} className="hover:underline">
           {name}
         </a>
       </Link>
       ) }
+      <hr />
     </div>
     )
 }
