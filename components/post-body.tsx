3ba0d49d88e33a479cbb211c093271afe37772a2
Nikolay Ponomarev <itsnikolay@gmail.com>
2020-12-21 00:02:57 +0300

    hightlight

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,10 +1,21 @@
 import markdownStyles from './markdown-styles.module.css'
+import 'highlight.js/styles/default.css';
+import hljs from 'highlight.js';
+import javascript from 'highlight.js/lib/languages/javascript';
+import ruby from 'highlight.js/lib/languages/ruby';
+import {useEffect} from 'react';
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('ruby', ruby);
 
 type Props = {
   content: string
 }
 
 const PostBody = ({ content }: Props) => {
+  useEffect(() => {
+    hljs.initHighlighting();
+  }, []);
+
   return (
     <div className="max-w-2xl mx-auto">
       <div
