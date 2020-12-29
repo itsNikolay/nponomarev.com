bd2c013e80736399e93d37f6333b02fd912dd132
Nikolay Ponomarev <itsnikolay@gmail.com>
2020-12-30 00:15:07 +0300

    Container grid

--- pages/posts/[category]/[slug].tsx
+++ pages/posts/[category]/[slug].tsx
@@ -32,31 +32,29 @@ const Post = ({ post, preview }: Props) => {
         {router.isFallback ? (
           <PostTitle>Loading…</PostTitle>
         ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {post.title}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-                <meta
-                  name="description"
-                  content={post.excerpt}
-                />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
+          <article>
+            <Head>
+              <title>
+                {post.title}
+              </title>
+              <meta property="og:image" content={post.ogImage.url} />
+              <meta
+                name="description"
+                content={post.excerpt}
               />
-              <PostBody content={post.content} />
-            </article>
-          </>
-      )}
-    </Container>
-  </Layout>
-)
+            </Head>
+            <PostHeader
+              title={post.title}
+              coverImage={post.coverImage}
+              date={post.date}
+              author={post.author}
+            />
+            <PostBody content={post.content} />
+          </article>
+        )}
+      </Container>
+    </Layout>
+  )
 }
 
 export default Post
