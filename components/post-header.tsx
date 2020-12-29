7fb522f733685cb35beff216ff4ed6b8f541f3b2
Nikolay Ponomarev <itsnikolay@gmail.com>
2020-12-30 00:42:17 +0300

    align date

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -17,10 +17,8 @@ const PostHeader = ({ title, coverImage, date }: Props) => {
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
       </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="flex justify-end mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
+      <div className="flex justify-end mb-6 text-lg">
+        <DateFormatter dateString={date} />
       </div>
     </>
   )
