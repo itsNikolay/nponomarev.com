8d952cfced949c5b69d06e0745e70a1e7e5c5cfc
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-01-07 23:21:50 +0300

    don't rename yandex

--- rename-html.sh
+++ rename-html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for file in $(find ./out/ -name "*.html" ! -name "index.html"); do
+for file in $(find ./out/ -name "*.html" ! -name "index.html" ! -name "yandex_a413ad079bd04bd4.html"); do
   echo $file
   newname=`echo $file | sed 's/\.html$//g'`
 
