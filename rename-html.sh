7ead63f453f7ffcf7f15421780cf8c4e9adc421e
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-01-08 01:01:54 +0300

    rename sh chnaged

--- rename-html.sh
+++ rename-html.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-for file in $(find ./out/ -name "*.html" ! -name "index.html" ! -name "yandex_a413ad079bd04bd4.html"); do
-  echo $file
+for file in $(find ./out -name "*.html" ! -name "index.html" ! -name "yandex_a413ad079bd04bd4.html"); do
   newname=`echo $file | sed 's/\.html$//g'`
 
-  mv $file $newname
+  echo "$file -> $newname"
+
+  touch "$newname"
+  cat "$file" > ${newname}
+  rm -f "$file"
 done
