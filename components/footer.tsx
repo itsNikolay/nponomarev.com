05ee3c121da6deff75ad00cb40aa548d2e5d33b6
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-01-12 22:07:51 +0300

    add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,14 @@
 import Container from './container'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className="border-t bg-accent-1 border-accent-2">
-      <Container>
-        <div className="flex flex-col items-center py-28 lg:flex-row">
-          <div className="flex flex-col items-center justify-center lg:flex-row lg:pl-4 lg:w-1/2">
-          </div>
-        </div>
-      </Container>
+    <footer className="py-10 text-center border-t bg-accent-1 border-accent-2">
+      <span>
+        Contact: <Link href="mailto:me@nponomarev.com" passHref={true} replace>
+          me@nponomarev.com
+        </Link>
+      </span>
     </footer>
   )
 }
