f392cf0d3429daac953f8ba299d4e5646b60e7f4
Nikolay Ponomarev <itsnikolay@gmail.com>
2021-01-12 22:15:40 +0300

    increase padding for code blocks

--- components/container.tsx
+++ components/container.tsx
@@ -9,7 +9,7 @@ const Container: FunctionComponent = ({ children }: Props) => {
     <div className="container justify-center px-5 m-auto mx-lg">
       <div className="grid grid-cols-5 gap-4">
         <div> </div>
-    <div className="mb-32 lg:col-span-3 md:col-span-5 sm:col-span-5">
+    <div className="lg:col-span-3 md:col-span-5 sm:col-span-5">
         {children}
     </div>
         <div> </div>
